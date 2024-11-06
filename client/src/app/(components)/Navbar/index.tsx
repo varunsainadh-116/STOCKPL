9943a95be092e592b3d8accb8d9331c3142b5c0a
Varun <korumillivarunsainadh@gmail.com>
2024-11-07 00:18:50 +0530

    s3 last commit

--- client/src/app/(components)/Navbar/index.tsx
+++ client/src/app/(components)/Navbar/index.tsx
@@ -58,12 +58,6 @@ const Navbar = () => {
               )}
             </button>
           </div>
-          <div className="relative">
-            <Bell className="cursor-pointer text-gray-500" size={24} />
-            <span className="absolute -top-2 -right-2 inline-flex items-center justify-center px-[0.4rem] py-1 text-xs font-semibold leading-none text-red-100 bg-red-400 rounded-full">
-              3
-            </span>
-          </div>
           <hr className="w-0 h-7 border border-solid border-l border-gray-300 mx-3" />
           <div className="flex items-center gap-3 cursor-pointer">
             <Image
