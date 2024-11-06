d4df7ec49273882de7efa1303ecc7f36ff39b2ea
Varun <korumillivarunsainadh@gmail.com>
2024-11-06 19:22:16 +0530

    initial commit

---
+++ server/ecosystem.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  apps: [
+    {
+      name: "StockPL",
+      script: "npm",
+      args: "run dev",
+      env: {
+        NODE_ENV: "development",
+        ENV_VAR1: "environment-variable",
+      },
+    },
+  ],
+};
